9ea66950087ca508374f82237b033877714c8830
ski <devilynmarra@gmail.com>
2025-06-08 20:16:07 -0300

    Fully working Virtual Joystick implemented

--- Script/Autoloads/GlobalScript.gd
+++ Script/Autoloads/GlobalScript.gd
@@ -1,9 +1,9 @@
 extends Node
 
-
+const DEBUG_RESOLUTION = Vector2(640, 1080)
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	DisplayServer.window_set_size(DEBUG_RESOLUTION)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
---
+++ Script/VirtualAnalog.cs
@@ -0,0 +1,109 @@
+using Godot;
+using System;
+
+public partial class VirtualAnalog : TouchScreenButton
+{
+	private const float DEADZONE = .05f;
+
+	[Export] public Sprite2D AnalogFG;
+
+	private Vector2 AnalogCenter = Vector2.Zero;
+	public bool AnalogPressing = false;
+	public Vector2 Direction = Vector2.Zero;
+	public override void _UnhandledInput(InputEvent @event)
+	{
+		_TestInputEventDrag(@event);
+		_TestInputEventTouch(@event);
+	}
+
+    public override void _Draw()
+    {
+		DrawCircle(AnalogCenter, AnalogCenter.X, new Color(255, 0, 0), false, 3);
+    }
+
+	public override void _Ready()
+	{
+		AnalogCenter = TextureNormal.GetSize() / 2;
+		AnalogFG.Position = AnalogCenter;
+	}
+	public override void _PhysicsProcess(double delta)
+	{
+		if (!AnalogFG.Position.Equals(AnalogCenter) && !AnalogPressing)
+		{
+			AnalogFG.Position = AnalogFG.Position.MoveToward(AnalogCenter, (float)delta * 2000);
+		}
+
+		_PressInputAction();
+		QueueRedraw();
+	}
+
+	private void _PressInputAction()
+	{
+
+		if (Direction.X > DEADZONE)
+			Input.ActionPress("walk_right", Direction.X);
+		else
+			Input.ActionRelease("walk_right");
+
+		if (Direction.X < -DEADZONE)
+			Input.ActionPress("walk_left", -Direction.X);
+		else
+			Input.ActionRelease("walk_left");
+
+		if (Direction.Y > DEADZONE)
+			Input.ActionPress("walk_down", Direction.Y);
+		else
+			Input.ActionRelease("walk_down");
+
+		if (Direction.Y < -DEADZONE)
+			Input.ActionPress("walk_up", -Direction.Y);
+		else
+			Input.ActionRelease("walk_up");
+
+		GD.Print(Direction);
+	}
+	private void _TestInputEventDrag(InputEvent e)
+	{
+		if (e is InputEventScreenDrag dragEvent && AnalogPressing)
+		{
+			_UpdateAnalogFGPosition(dragEvent.Position);
+		}
+	}
+	private void _TestInputEventTouch(InputEvent e)
+	{
+		if (e is InputEventScreenTouch touchEvent)
+		{
+			if (touchEvent.IsReleased())
+			{
+				AnalogPressing = false;
+				Direction = Vector2.Zero;
+			}
+			else if (!touchEvent.IsReleased() && new Rect2(Position, TextureNormal.GetSize()).HasPoint(touchEvent.Position))
+			{
+				_UpdateAnalogFGPosition(touchEvent.Position);
+				AnalogPressing = true;
+			}
+		}
+	}
+
+	private void _UpdateAnalogFGPosition(Vector2 newFGPos)
+	{
+			Vector2 relativeToCenterEventPosition = newFGPos - Position - AnalogCenter;
+
+			Direction = relativeToCenterEventPosition.Normalized();
+			float analogLenght = relativeToCenterEventPosition.Length() / (AnalogCenter.X);
+
+			AnalogFG.Position = relativeToCenterEventPosition + AnalogCenter;
+
+			if (analogLenght > 1f)
+			{
+				AnalogFG.Position = AnalogCenter + Direction * AnalogCenter;
+			}
+
+			Direction = Direction * Mathf.Clamp(analogLenght, 0, 1);
+	}
+	private Vector2 GetCenteredFGPosition()
+	{
+		return AnalogFG.Position - TextureNormal.GetSize() / 2;
+	}
+}
