1b1faa2564e03e2a83c4dafdfe999616463f13a4
ski <devilynmarra@gmail.com>
2025-06-21 15:50:45 -0300

    Added ids for items at the GlobalScript.gd

--- Script/Autoloads/GlobalScript.gd
+++ Script/Autoloads/GlobalScript.gd
@@ -3,6 +3,11 @@ extends Node
 const DEBUG_MODE = true
 const TILESIZE = 128
 
+const ITEM_IDS = {
+	"coffee_beam": 0,
+	"coffee_cup": 1,
+	"milk":2
+}
 
 func is_debug_mode() -> bool:
 	return DEBUG_MODE
